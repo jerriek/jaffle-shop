9141b4707cf39c98c8df70fe12df39f186d25c30
Jerrie Kumalah <jerrie.kumalah@fishtownanalytics.com>
2024-03-15 14:56:53 +0000

    sample work for client

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,26 +1,6 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
+-- put this in macros/get_custom_schema.sql
 
-    {% set default_schema = target.schema %}
-
-    {# seeds go in a global `raw` schema #}
-    {% if node.resource_type == 'seed' %}
-        {{ custom_schema_name | trim }}
-
-    {# non-specified schemas go to the default target schema #}
-    {% elif custom_schema_name is none %}
-        {{ default_schema }}
-
-
-    {# specified custom schema names go to the schema name prepended with the the default schema name in prod (as this is an example project we want the schemas clearly labeled) #}
-    {% elif target.name == 'prod' %}
-        {{ default_schema }}_{{ custom_schema_name | trim }}
-
-    {# specified custom schemas go to the default target schema for non-prod targets #}
-    {% else %}
-        {{ default_schema }}
-    {% endif %}
-
-{% endmacro %} 
-
-
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {{ generate_schema_name_for_env(custom_schema_name, node) }}
+{%- endmacro %}
 
