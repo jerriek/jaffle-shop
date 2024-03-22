7ba9e79c31def95c18f54757ec58a27f0133bbfa
Jerrie Kumalah <jerrie.kumalah@fishtownanalytics.com>
2024-03-22 20:30:16 +0000

    linting test

--- models/no_source.sql
+++ models/no_source.sql
@@ -1,9 +1,15 @@
-
 with no_source as (
-select 1 as od_id, 'jump' as asset_type 
-union all
-select 2 as od_id, 'sit' as asset_type 
-union all
-select 3 as od_id, 'stand' as asset_type 
+    select
+        1 as od_id,
+        'jump' as asset_type
+    union all
+    select
+        2 as od_id,
+        'sit' as asset_type
+    union all
+    select
+        3 as od_id,
+        'stand' as asset_type
 )
+
 select * from no_source
