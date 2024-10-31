1d7c2da3a08147fd91960dfac6045d77dd54d4e4
Jerrie Kumalah <jerrie.kumalah@fishtownanalytics.com>
2024-10-31 18:38:46 +0000

    overriding the ref macro

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -3,3 +3,40 @@
 where {{ column_name }} >= dateadd('day', -{{ dev_days }}, current_date)
 {% endif %}
 {%- endmacro %}
+
+
+{# overriding the ref macro to test this out at the project level #}
+
+{% macro ref() %}
+
+-- extract user-provided positional and keyword arguments
+{% set version = kwargs.get('version') or kwargs.get('v') %}
+{% set packagename = none %}
+{%- if (varargs | length) == 1 -%}
+    {% set modelname = varargs[0] %}
+{%- else -%}
+    {% set packagename = varargs[0] %}
+    {% set modelname = varargs[1] %}
+{% endif %}
+
+-- call builtins.ref based on provided positional arguments
+{% set rel = None %}
+{% if packagename is not none %}
+    {% set rel = builtins.ref(packagename, modelname, version=version) %}
+{% else %}
+    {% set rel = builtins.ref(modelname, version=version) %}
+{% endif %}
+
+-- finally, override the database name with "dev"
+{% set newrel = rel.replace_path(database="analytics") %}
+
+-- additional logic to check environment and apply data limit if needed
+{% set filter = var('filter') %}
+{%- if env_var('DBT_ENVIRONMENT') in ('dev', 'ci') and var('limit', true) -%}
+    {% set limited_rel %} (select * from {{ newrel }} {{ filter }}) {% endset %}
+    {% do return(limited_rel) %}
+{% else %}
+    {% do return(newrel) %}
+{%- endif -%}
+
+{% endmacro %}
